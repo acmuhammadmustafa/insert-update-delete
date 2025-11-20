c150c9e0b53fb2b67864f2b2e93f7e92d3cc860e
origin <acmuhammadmustafa@gmail.com>
2025-11-20 15:49:04 -0500

    completed question 1

---
+++ 01_supplier_added.sql
@@ -0,0 +1,4 @@
+use northwind;
+
+insert into suppliers (SupplierID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax, HomePage)
+values(30, 'MuhammaSupplier', 'Muhammad', 'Boss', '123 Magic Lane', 'Charlotte', 'NC', 3212, 'USA', 0981291021, 0980920990, 'accmuhammad.com')
